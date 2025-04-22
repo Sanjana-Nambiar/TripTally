0165fb449e121e032ea0dd8697c1762db736dbf5
Sanjana-Nambiar <sanjuvn1@gmail.com>
2025-04-22 18:18:23 +0400

    vercel

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -275,9 +275,9 @@ app.get('*', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 export default app;
